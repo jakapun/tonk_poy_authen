fe190c6d0a372be134249a65ef8de7ab7a4defbc
jakapun <jakapun.c@gmail.com>
2019-03-29 11:55:42 +0700

    save value from form

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,11 +6,27 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Explicit //ประกาศตัวแปร
+
+  String nameString, emailString, passwordString;
+
+  // Key
+
+  final formKey = GlobalKey<FormState>();
+
   //name
   Widget nameTextField() {
     return TextFormField(
       decoration:
           InputDecoration(labelText: 'Name: ', hintText: 'Type Your Name'),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'please type name?';
+        }
+      },
+      onSaved: (String value) {
+        nameString = value;
+      },
     );
   }
 
@@ -19,6 +35,14 @@ class _RegisterState extends State<Register> {
     return TextFormField(
       decoration: InputDecoration(
           labelText: 'Email Address :', hintText: 'your@email.com'),
+      validator: (String value) {
+        if (!((value.contains('@') && value.contains('.')))) {
+          return 'type email format yourmail@name.com';
+        }
+      },
+      onSaved: (String value) {
+        emailString = value;
+      },
     );
   }
 
@@ -28,46 +52,65 @@ class _RegisterState extends State<Register> {
     return TextFormField(
       decoration: InputDecoration(
           labelText: 'Password :', hintText: 'more 6 charactor'),
+      validator: (String value) {
+        if (value.length <= 5) {
+          return 'Please Type Password > 5 digit';
+        }
+      },
+      onSaved: (String value) {
+        passwordString = value;
+      },
     );
   }
 
-  void clickCloudUpload(){
-
+  void clickCloudUpload() {
     print('click upload');
+    print(formKey.currentState.validate());
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      print(
+          'name => $nameString, email => $emailString, password => $passwordString');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false, //keyboard ทับ scaffold
-      appBar: AppBar(
-        title: Text('Register'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.cloud_upload),
-            tooltip: 'Upload To Server', //press 3 second
-            onPressed: () {clickCloudUpload();},
-          )
-        ],
-      ),
-      // body: Text('this is body'),
-      body: Container(
-        padding: EdgeInsets.all(50.0),
-        child: Container(
-          padding: EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30.0),
-              border: Border.all(
-                  width: 2.0, color: Colors.blue, style: BorderStyle.solid)),
-          child: Column(
-            children: <Widget>[
-              nameTextField(),
-              emailTextfield(),
-              passwordTextfield()
-            ],
-          ),
+        resizeToAvoidBottomPadding: false, //keyboard ทับ scaffold
+        appBar: AppBar(
+          title: Text('Register'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.cloud_upload),
+              tooltip: 'Upload To Server', //press 3 second
+              onPressed: () {
+                clickCloudUpload();
+              },
+            )
+          ],
         ),
-      ),
-    );
+        // body: Text('this is body'),
+        body: Form(
+          key: formKey,
+          child: Container(
+            padding: EdgeInsets.all(50.0),
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  border: Border.all(
+                      width: 2.0,
+                      color: Colors.blue,
+                      style: BorderStyle.solid)),
+              child: Column(
+                children: <Widget>[
+                  nameTextField(),
+                  emailTextfield(),
+                  passwordTextfield()
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
